166d7d9b8fa9fe6c2f6e78a95f18409dc134c486
riyakushwaha <reakushwaha@gmail.com>
2020-09-24 10:30:22 +0530

    UpsideButterfly

---
+++ Pattern1.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Pattern1 {
+
+    public static  void main(String []args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+    System.out.println(n/2);
+        int values, spaces;
+
+        for(int i =0; i<n ; i++)
+        {
+            if(i>=n/2)
+            {
+                spaces = n-1-i;
+            }
+            else
+            {
+                spaces = i;
+            }
+
+            values = n-(2*spaces);
+
+            int j = spaces;
+            while(j>0)
+            {
+                System.out.print(" ");
+                j--;
+            }
+
+            if(i==0 || i>n/2)
+            {
+                while(values>0)
+                {
+                    System.out.print("*");
+                    values--;
+                }
+            }
+            else
+            {
+                for(j = values; j>0; j--)
+                {
+                    if(j==1 || j==values)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+            }
+
+            System.out.println("");
+        }
+
+    }
+
+}
