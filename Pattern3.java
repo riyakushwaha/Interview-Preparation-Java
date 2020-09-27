69b639a0cc27f3d51b83720f32ce4fe9fb14bd32
riyakushwaha <reakushwaha@gmail.com>
2020-09-27 08:34:46 +0530

    Pattern3

---
+++ Pattern3.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Pattern3 {
+
+    public static  void main(String []args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); //n
+
+        int spaces, values, temp;
+
+        for(int i=0; i<n; i++)
+        {
+            values= n-i;
+            spaces = 2*i;
+            temp = values;
+
+
+            while(temp>0)
+            {
+                System.out.print("*");
+                temp--;
+            }
+            while(spaces>0)
+            {
+                System.out.print(" ");
+                spaces--;
+            }
+
+            while(values>0)
+            {
+                System.out.print("*");
+                values--;
+            }
+
+            System.out.println("");
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            values= i+1;
+            spaces = 2*(n-values);
+            temp = values;
+
+
+            while(temp>0)
+            {
+                System.out.print("*");
+                temp--;
+            }
+            while(spaces>0)
+            {
+                System.out.print(" ");
+                spaces--;
+            }
+
+            while(values>0)
+            {
+                System.out.print("*");
+                values--;
+            }
+
+            System.out.println("");
+        }
+
+
+
+    }
+
+}
